4a34cff0312d064f45330cf33c7dbc470b2f0f41
jess2 <je_ss2@naver.com>
2017-11-29 19:44:12 +0900

    icon page생성

---
+++ src/pages/icon/icon.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'icon.html'
+})
+export class IconPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}

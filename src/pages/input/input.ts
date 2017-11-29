0bcf3b224abcf91c6ed9605525f0f1d1f97e8201
jess2 <je_ss2@naver.com>
2017-11-29 19:44:21 +0900

    input page생성

---
+++ src/pages/input/input.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'input.html'
+})
+export class InputPage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+}
